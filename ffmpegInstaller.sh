b31f8f3a26253aa25b890ed4cc4285ed831c9717
Febin Baiju <febin129@gmail.com>
2015-12-27 18:15:02 +0530

    alias replaced with make commands

--- ffmpegInstaller.sh
+++ ffmpegInstaller.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-alias mk='make && make install && make distclean && cd ~/ffmpeg_sources'
 chk_root(){
     USR=`id -u`
     if [ "$USR" != 0 ]
@@ -49,7 +48,11 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         autoreconf -fiv
         ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
         config_chk Yasm
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
+
         chk_last Yasm
         
         echo "Installing libx264...."
@@ -58,7 +61,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd x264
         PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
         config_chk libx264
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last libx264
         
         echo "Installing fdk-aac..."
@@ -68,7 +74,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         autoreconf -fiv
         ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
         config_chk fdk-aac
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last fdk-aac
         
         echo "Installing libmp3lame..."
@@ -80,7 +89,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd lame-3.99.5
         ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
         config_chk libmp3lame
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last libmp3lame
         
         echo "Installing libvpx..."
@@ -89,7 +101,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd libvpx
         ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
         config_chk libvpx
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last libvpx
         
         echo "Installing opencoreamr..."
@@ -101,7 +116,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd opencore-amr-0.1.3
         ./configure
         config_chk opencore
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last opencore
         
         echo "Installing xvidcore..."
@@ -113,7 +131,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd xvidcore-1.3.2/build/generic
         ./configure
         config_chk xvidcore
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last xvidcore
         
         echo "Installing aac-encoder..."
@@ -125,7 +146,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd vo-aacenc-0.1.3
         ./configure
         config_chk aac-encoder
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last aac-encoder
         
         echo "Finished Installing Libraries..."
@@ -138,7 +162,10 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd ffmpeg
         PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libopencore-amrnb --enable-version3 --enable-libvo_aacenc --enable-libmp3lame --enable-libfdk_aac --enable-libx264 --enable-libxvid
         config_chk ffmpeg
-        mk
+        make
+        make install
+        make distclean
+        cd ~/ffmpeg_sources
         chk_last ffmpeg
         
         echo "---------------FFMPEG INSTALLATION SUCCESS------------------"
