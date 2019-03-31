19fc853c652d7aa8de70ab729a65cc833a995151
Febin Baiju <mysticbits2013@gmail.com>
2019-03-31 11:57:35 +0530

    modified installer added

--- ffmpegInstaller.sh
+++ ffmpegInstaller.sh
@@ -37,6 +37,7 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         owd=$(pwd)
 	    mkdir -p /usr/ffmpeg2017/
         export HOME=/usr/ffmpeg2017/ #  Global Declaration
+        export PATH=$PATH:/usr/ffmpeg2017/bin
         echo "Making installation directory..."
         mkdir ~/ffmpeg_sources
         
@@ -55,9 +56,8 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         
         echo "Installing Yasm assembler..."
         cd ~/ffmpeg_sources
-        git clone --depth 1 https://github.com/yasm/yasm.git
-        cd yasm
-        autoreconf -fiv
+        tar -xzvf $owd/yasm-1.3.0.tar.gz
+        cd yasm-1.3.0
         ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
         config_chk Yasm
         make
@@ -69,9 +69,9 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         
         echo "Installing libx264...."
         cd ~/ffmpeg_sources
-        git clone --depth 1 http://git.videolan.org/git/x264.git
-        cd x264
-        PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+        tar -xJf $owd/x264.tar.xz
+        cd "x264-snapshot-20160220-2245-stable"
+        PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic
         config_chk libx264
         make
         make install
@@ -81,7 +81,7 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         
         echo "Installing fdk-aac..."
         cd ~/ffmpeg_sources
-        git clone --depth 1 https://github.com/Distrotech/fdk-aac.git
+        tar -xJf $owd/fdk-aac.tar.xz
         cd fdk-aac
         autoreconf -fiv
         ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
@@ -104,24 +104,12 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd ~/ffmpeg_sources
         chk_last libmp3lame
         
-        echo "Installing libvpx..."
-        cd ~/ffmpeg_sources
-        git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-        cd libvpx
-        ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
-        config_chk libvpx
-        make
-        make install
-        make distclean
-        cd ~/ffmpeg_sources
-        chk_last libvpx
-        
         echo "Installing opencoreamr..."
         cd ~/ffmpeg_sources
         tar -xzvf $owd/opencore-amr-0.1.3.tar.gz
         cd opencore-amr-0.1.3
-        ./configure
-        config_chk opencore
+        ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+        config_chk opencore 
         make
         make install
         make distclean
@@ -132,7 +120,7 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd ~/ffmpeg_sources
         tar -xzvf $owd/xvidcore-1.3.2.tar.gz
         cd xvidcore/build/generic
-        ./configure
+        ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
         config_chk xvidcore
         make
         make install
@@ -144,7 +132,7 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         cd ~/ffmpeg_sources
         tar -xzvf $owd/vo-aacenc-0.1.3.tar.gz
         cd vo-aacenc-0.1.3
-        ./configure
+        ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
         config_chk aac-encoder
         make
         make install
@@ -158,7 +146,7 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         echo "Installing FFMPEG...."
         
         cd ~/ffmpeg_sources
-       tar -xjvf $owd/ffmpeg-2.8.2.tar.bz2
+        tar -xJf $owd/ffmpeg-2.8.2.tar.xz
         cd ffmpeg-2.8.2
         PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libopencore-amrnb --enable-version3 --enable-libfdk_aac --enable-libvo-aacenc --enable-libmp3lame --enable-libx264 --enable-libxvid
         config_chk ffmpeg
@@ -171,19 +159,23 @@ echo "---------------------Beginning FFMPEG Installation------------------------
         
 		echo "REMOVING LIBRARY FILES"
 		
-		rm -f $owd/lame-3.99.5.tar.gz
-		rm -f $owd/opencore-amr-0.1.3.tar.gz
-		rm -f $owd/xvidcore-1.3.2.tar.gz
-		rm -f $owd/vo-aacenc-0.1.3.tar.gz
-		rm -rf $owd/ffmpeg-2.8.2.tar.bz2
+		rm -f $owd/lame-3.99.5.tar.xz
+		rm -f $owd/opencore-amr-0.1.3.tar.xz
+		rm -f $owd/xvidcore-1.3.2.tar.xz
+		rm -f $owd/vo-aacenc-0.1.3.tar.xz
+		rm -f $owd/ffmpeg-2.8.2.tar.xz
+        rm -f $owd/fdk-aac.tar.xz
+        rm -f $owd/x264.tar.xz
+        rm -f $owd/yasm-1.3.0.tar.gz
+        rm -f $owd/nasm-2.14.02.tar.xz
+
 		
 		echo "REMOVING LIBRARY FOLDERS"
 		
 		rm -rf yasm
-		rm -rf x264
+		rm -rf "x264-snapshot-20160220-2245-stable"
 		rm -rf fdk-aac
 		rm -rf lame-3.99.5
-		rm -rf libvpx
 		rm -rf opencore-amr-0.1.3
 		rm -rf xvidcore
 		rm -rf vo-aacenc-0.1.3
@@ -192,9 +184,18 @@ echo "---------------------Beginning FFMPEG Installation------------------------
 		
 		echo "ADDING LIBRARY PATH TO LDCONFIG"
 		
-		echo "/usr/local/lib/">>/etc/ld.so.conf
+		echo "/usr/ffmpeg2017/ffmpeg_build/lib/">>/etc/ld.so.conf
 		ldconfig
-		
 
-        echo "---------------FFMPEG INSTALLATION SUCCESS------------------"
-        history -c
+        cd $HOME/bin
+        mkdir 2017
+        mv ffmpeg 2017/ffmpeg2017
+        mv ffserver 2017/ffserver2017
+        mv ffprobe 2017/ffprobe2017
+
+        echo "ADDING ffmpeg2017 TO GLOBAL PATH"
+
+        cd 2017
+        PATH=$PATH:$(pwd)
+
+        echo "---------------FFMPEG INSTALLER EXITED------------------"
